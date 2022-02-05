d330916dd5e82d4537de12f13f734274b61ca017
Luke Petrolekas <reclarion@gmail.com>
2022-02-05 18:14:51 -0500

    Organize page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import './App.css';
-import Handform from './HandForm';
+import "./App.css";
+import Handform from "./HandForm";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h1>Simplified Poker Game Solver</h1>
-        <p>Given an ante/check value, a bet value, and the number of cards in a Simplified Poker Game, this program will give the optimal strategies for that game.</p>
+      <article className="container">
+        <header className="p-3 pb-md-4 mx-auto text-center">
+          <h1 className="display-4 fw-normal">Simplified Poker Game Solver</h1>
+          <p className="fs-5 text-muted">
+            Poker patter:
+            <br />
+            Mathematics has been fascinated by poker. While a game of Texas hold
+            'em or 7 Card Stud are fun, mathematics has chosen to focus on
+            simpler games and has discovered several optimal play strategies.
+          </p>
+        </header>
+        <section>
+          <p>
+            Given an ante/check value, a bet value, and the number of cards in a
+            Simplified Poker Game, this program will give the optimal strategies
+            for that game.
+          </p>
+        </section>
         <Handform />
-      </div>
+      </article>
     </div>
   );
 }
