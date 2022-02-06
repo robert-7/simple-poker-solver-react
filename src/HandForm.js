a2c293f3adbf6e5d822886a6d547c35302d420ad
Robert Lech <robert.lech123@gmail.com>
2022-02-05 21:44:02 -0500

    Cleans up the UI (#1)
    * WIP: some cleanup
    
    * Organize page
    
    * Add a toggle to switch game type
    
    * each value in form connects to value in state
    
    * nitpick
    
    Co-authored-by: Luke Petrolekas <reclarion@gmail.com>

--- src/HandForm.js
+++ src/HandForm.js
@@ -1,64 +1,104 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { ToggleButton } from "react-bootstrap";
+import { ToggleButtonGroup } from "react-bootstrap";
 
 class HandForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {anteValue: 0, betValue: 0, numCards: 0, answer: []};
-  
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      algorithm: 1,
+      anteValue: 0,
+      betValue: 0,
+      numCards: 0,
+      answer: [],
+    };
 
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.setRadioValue = this.setRadioValue.bind(this);
+    this.setAnteValue = this.setAnteValue.bind(this);
+    this.setBetValue = this.setBetValue.bind(this);
+    this.setNumCards = this.setNumCards.bind(this);
+  }
 
-    render() {
-      return (
-        <div>
-            <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-            <label className="control-label col-sm-2">Enter Ante Value:</label>
-            <div className="col-sm-10">
-                <input className="form-control" id="ante" placeholder="Enter ante value" value={this.state.anteValue}></input>
-            </div>
-            </div>
-            <div className="form-group">
-            <label className="control-label col-sm-2">Enter Bet Value:</label>
-            <div className="col-sm-10">
-                <input className="form-control" id="bet" placeholder="Enter bet value" value={this.state.betValue}></input>
-            </div>
-            </div>
-            <div className="form-group">
-            <label className="control-label col-sm-2">Enter # of Cards:</label>
-            <div className="col-sm-10">
-                <input className="form-control" id="numCards" placeholder="Enter # of cards" value={this.state.numCards}></input>
-            </div>
-            </div>
-            <div className="form-group">
-            <div className="col-sm-offset-2 col-sm-10">
-                <button type="submit">Submit</button>
-            </div>
-            </div>
-        </form>
+  handleSubmit(event) {
+    // why should we have to disable something that should be auto-disabled?
+    event.preventDefault();
+    alert("A name was submitted: " + this.state.value);
+  }
+
+  setRadioValue(value) {
+    this.setState({ algorithm: value });
+  }
+
+  setAnteValue(event) {
+    this.setState({ anteValue: event.target.value });
+  }
+
+  setBetValue(event) {
+    this.setState({ betValue: event.target.value });
+  }
+
+  setNumCards(event) {
+    this.setState({ numCards: event.target.value })
+  }
+
+  render() {
+    return (
+      <section>
+        <Form onSubmit={this.handleSubmit}>
+          <ToggleButtonGroup
+            name="games"
+            type="radio"
+            value={this.state.algorithm}
+            onChange={this.setRadioValue}
+          >
+            <ToggleButton id="tbg-radio-1" value={1}>
+              Borel
+            </ToggleButton>
+            <ToggleButton id="tbg-radio-2" value={2}>
+              Von Neumann
+            </ToggleButton>
+            <ToggleButton id="tbg-radio-3" value={3}>
+              Game3 3
+            </ToggleButton>
+          </ToggleButtonGroup>
+
+          <Form.Group className="mb-3" controlId="handform.Ante">
+            <Form.Label>Ante Value:</Form.Label>
+            <Form.Control type="number" placeholder="0" onChange={this.setAnteValue}/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="handform.Bet">
+            <Form.Label>Bet Value:</Form.Label>
+            <Form.Control type="number" placeholder="0" onChange={this.setBetValue}/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="handform.Deck">
+            <Form.Label>Number of Cards In Deck:</Form.Label>
+            <Form.Control type="number" placeholder="0" onChange={this.setNumCards}/>
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
 
         <div id="table">
-            <h2>Striped Rows</h2>
-            <table className="table table-striped">
+          <h2>Results</h2>
+          <table className="table table-striped">
             <thead>
-                <tr>
+              <tr>
                 <th>Player 1 Strategy</th>
                 <th>Player 2 Strategy</th>
                 <th>Player 1's Payoff</th>
-                </tr>
+              </tr>
             </thead>
-            <tbody id="rows">
-            </tbody>
-            </table>
+            <tbody id="rows"></tbody>
+          </table>
         </div>
-      </div>
-      );
-    }
+      </section>
+    );
   }
+}
 
-  export default HandForm;
+export default HandForm;
