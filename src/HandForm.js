2c8ed90b32c5876657b09f4ddbc5bfb767667d94
Luke Petrolekas <reclarion@gmail.com>
2022-02-06 13:40:47 -0500

    formatting

--- src/HandForm.js
+++ src/HandForm.js
@@ -42,79 +42,95 @@ class HandForm extends React.Component {
   }
 
   setNumCards(event) {
-    this.setState({ numCards: event.target.value })
+    this.setState({ numCards: event.target.value });
   }
 
   render() {
     return (
       <section>
-          <section id="options" className="p-3">
-          <header><h2>Game</h2></header>
-        <Form onSubmit={this.handleSubmit}>
-          <ToggleButtonGroup
-            name="games"
-            type="radio"
-            value={this.state.algorithm}
-            onChange={this.setRadioValue}
-          >
-            <ToggleButton id="tbg-radio-1" value={1}>
-              Borel
-            </ToggleButton>
-            <ToggleButton id="tbg-radio-2" value={2}>
-              Von Neumann
-            </ToggleButton>
-            <ToggleButton id="tbg-radio-3" value={3}>
-              Game3 3
-            </ToggleButton>
-          </ToggleButtonGroup>
+        <section id="options" className="p-3">
+          <header>
+            <h2>Game</h2>
+          </header>
+          <Form onSubmit={this.handleSubmit}>
+            <ToggleButtonGroup
+              name="games"
+              type="radio"
+              value={this.state.algorithm}
+              onChange={this.setRadioValue}
+            >
+              <ToggleButton id="tbg-radio-1" value={1}>
+                Borel
+              </ToggleButton>
+              <ToggleButton id="tbg-radio-2" value={2}>
+                Von Neumann
+              </ToggleButton>
+              <ToggleButton id="tbg-radio-3" value={3}>
+                Game3 3
+              </ToggleButton>
+            </ToggleButtonGroup>
 
-          <Form.Group className="mb-3" controlId="handform.Ante">
-            <Form.Label>Ante Value:</Form.Label>
-            <Form.Control type="number" placeholder="0" onChange={this.setAnteValue}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="handform.Bet">
-            <Form.Label>Bet Value:</Form.Label>
-            <Form.Control type="number" placeholder="0" onChange={this.setBetValue}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="handform.Deck">
-            <Form.Label>Number of Cards In Deck:</Form.Label>
-            <Form.Control type="number" placeholder="0" onChange={this.setNumCards}/>
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="handform.Ante">
+              <Form.Label>Ante Value:</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="0"
+                onChange={this.setAnteValue}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="handform.Bet">
+              <Form.Label>Bet Value:</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="0"
+                onChange={this.setBetValue}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="handform.Deck">
+              <Form.Label>Number of Cards In Deck:</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="0"
+                onChange={this.setNumCards}
+              />
+            </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </section>
 
         <section id="results" className="p-3">
-          <header><h2>Results</h2></header>
+          <header>
+            <h2>Results</h2>
+          </header>
           <Table striped bordered hover size="sm">
             <thead>
-                <tr>
+              <tr>
                 <th>Player 1 - Strategy</th>
                 <th>Player 2 - Strategy</th>
                 <th>Player 1 - Payoff</th>
-                </tr>
+              </tr>
             </thead>
             <tbody>
-                <tr>
+              <tr>
                 <td>1</td>
                 <td>Mark</td>
                 <td>Otto</td>
-                </tr>
-                <tr>
+              </tr>
+              <tr>
                 <td>2</td>
                 <td>Jacob</td>
                 <td>Thornton</td>
-                </tr>
-                <tr>
+              </tr>
+              <tr>
                 <td>2</td>
                 <td>Jacob</td>
                 <td>Thornton</td>
-                </tr>
+              </tr>
             </tbody>
-            </Table>
+          </Table>
         </section>
       </section>
     );
