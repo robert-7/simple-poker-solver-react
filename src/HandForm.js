31c1006161ce88d6e079ee5eff3f601c4470cdd8
Robert Lech <robert.lech123@gmail.com>
2023-03-28 22:03:23 -0400

    chore: cleans up the UI even more

--- src/HandForm.js
+++ src/HandForm.js
@@ -3,6 +3,7 @@ import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 import { ToggleButton } from "react-bootstrap";
 import { ToggleButtonGroup } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 
 class HandForm extends React.Component {
   constructor(props) {
@@ -12,7 +13,14 @@ class HandForm extends React.Component {
       anteValue: 0,
       betValue: 0,
       numCards: 0,
-      answer: [],
+      results: [
+        {
+          key: "unique row key",
+          p1: "an",
+          p2: "example",
+          p1payoff: "row",
+        },
+      ],
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,7 +33,6 @@ class HandForm extends React.Component {
   handleSubmit(event) {
     // why should we have to disable something that should be auto-disabled?
     event.preventDefault();
-    alert("A name was submitted: " + this.state.value);
   }
 
   setRadioValue(value) {
@@ -41,61 +48,89 @@ class HandForm extends React.Component {
   }
 
   setNumCards(event) {
-    this.setState({ numCards: event.target.value })
+    this.setState({ numCards: event.target.value });
   }
 
   render() {
     return (
       <section>
-        <Form onSubmit={this.handleSubmit}>
-          <ToggleButtonGroup
-            name="games"
-            type="radio"
-            value={this.state.algorithm}
-            onChange={this.setRadioValue}
-          >
-            <ToggleButton id="tbg-radio-1" value={1}>
-              Borel
-            </ToggleButton>
-            <ToggleButton id="tbg-radio-2" value={2}>
-              Von Neumann
-            </ToggleButton>
-            <ToggleButton id="tbg-radio-3" value={3}>
-              Game3 3
-            </ToggleButton>
-          </ToggleButtonGroup>
+        <section id="options" className="p-3">
+          <header>
+            <h2>Game</h2>
+          </header>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group className="mb-3" controlId="handform.Type">
+              <Form.Label>Game Type:</Form.Label>
+              <br />
+              <ToggleButtonGroup
+                name="games"
+                type="radio"
+                value={this.state.algorithm}
+                onChange={this.setRadioValue}
+              >
+                <ToggleButton id="tbg-radio-1" value={1}>
+                  Borel
+                </ToggleButton>
+                <ToggleButton id="tbg-radio-2" value={2}>
+                  von Neumann
+                </ToggleButton>
+              </ToggleButtonGroup>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="handform.Ante">
-            <Form.Label>Ante Value:</Form.Label>
-            <Form.Control type="number" placeholder="0" onChange={this.setAnteValue}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="handform.Bet">
-            <Form.Label>Bet Value:</Form.Label>
-            <Form.Control type="number" placeholder="0" onChange={this.setBetValue}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="handform.Deck">
-            <Form.Label>Number of Cards In Deck:</Form.Label>
-            <Form.Control type="number" placeholder="0" onChange={this.setNumCards}/>
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="handform.Ante">
+              <Form.Label>Ante Value:</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="0"
+                onChange={this.setAnteValue}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="handform.Bet">
+              <Form.Label>Bet Value:</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="0"
+                onChange={this.setBetValue}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="handform.Deck">
+              <Form.Label>Number of Cards In Deck:</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="0"
+                onChange={this.setNumCards}
+              />
+            </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </section>
 
-        <div id="table">
-          <h2>Results</h2>
-          <table className="table table-striped">
+        <section id="results" className="p-3">
+          <header>
+            <h2 className="pb-2">Results</h2>
+          </header>
+          <Table striped bordered hover size="sm">
             <thead>
               <tr>
-                <th>Player 1 Strategy</th>
-                <th>Player 2 Strategy</th>
-                <th>Player 1's Payoff</th>
+                <th>Player 1 - Strategy</th>
+                <th>Player 2 - Strategy</th>
+                <th>Player 1 - Payoff</th>
               </tr>
             </thead>
-            <tbody id="rows"></tbody>
-          </table>
-        </div>
+            <tbody>
+              {this.state.results.map((r, index) => (
+                <tr key={index}>
+                  <td>{r.p1}</td>
+                  <td>{r.p2}</td>
+                  <td>{r.p1payoff}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </section>
       </section>
     );
   }
